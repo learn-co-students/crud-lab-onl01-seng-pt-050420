79e7df15d174bfd9dbae1d68d3af0d5d48bf014c
danielkwon89 <danielkwon89@gmail.com>
2021-02-14 14:19:50 -0500

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  state = {
+    text: ""
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state)
+    this.setState({
+      text: ""
+    })
+  }
+
   render() {
-    return (
+    return (  
       <div>
-        Restaurant Input
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <label>Restaurant: </label><br />
+          <input type="text" value={this.state.text} onChange={e => this.handleChange(e)} /><br />
+          <input type="submit" />
+        </form>
+        {this.state.text}
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,30 @@
 import cuid from 'cuid';
 
-export default function manageRestaurants(state, action) {
+const manageRestaurants = (state = { restaurants: [], reviews: [] }, action) => {
+    switch (action.type) {
 
+        case "ADD_RESTAURANT":
+            let newRestaurant = {...action.restaurant, id: cuid()}
+            return {
+                ...state,
+                restaurants: [...state.restaurants, newRestaurant]
+            }
+
+        case "DELETE_RESTAURANT":
+            return { ...state, 
+                restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id) 
+            }
+
+        case "ADD_REVIEW":
+            debugger
+            let newReview = {...action.review, restaurantId: cuid()}
+            return { ...state,
+                reviews: [...state.reviews, newReview]
+            }
+
+        default:
+            return state
+    }
 }
+
+export default manageRestaurants
