d91bf1be2a5b0cff4d4f72cfadb2e90eae84fae3
ParishBen <ParishBen13@gmail.com>
2021-02-17 16:59:51 -0500

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
-  render() {
+  // const restaurantList = restaurants.map(restaurant => {
+    //   return (
+      //     <Restaurant
+      //         key={restaurant.id}
+      //         restaurant={restaurant}
+      //         deleteRestaurant={deleteRestaurant}
+      //     />
+      //   )
+      // });
+      render() {
+        console.log(this.props)
+    const { reviews, restaurantId, deleteReview } = this.props;
+    const associatedReviews = reviews.filter(review => review.restaurantId === restaurantId);
+    
+    const reviewList = associatedReviews.map((review, index) => {
+      return <Review key={index} review={review} deleteReview={deleteReview} />
+    })
     return (
       <ul>
-        Reviews
+        {reviewList}
       </ul>
     );
   }
