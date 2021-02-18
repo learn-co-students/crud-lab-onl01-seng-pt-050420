828ce0728a9d04a59aa4c2d405ac9ec381cb9661
Oscar Ore <oscar.ore2011@gmail.com>
2021-02-17 21:00:55 -0500

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -5,16 +5,15 @@ class Restaurant extends Component {
 
 
   render() {
+
     const { restaurant } = this.props;
 
     return (
-      <div>
-        <li>
-          {restaurant.text}
-          <button> X </button>
-          <ReviewsContainer restaurant={restaurant}/>
+        <li key={restaurant.id} id={restaurant.id}>
+          {restaurant.text}{" "}
+          <button onClick={id => this.props.deleteRestaurant(restaurant.id)}> X </button>
+          <ReviewsContainer restaurant={restaurant} addReview={this.props.addReview} deleteReview={this.props.deleteReview} />
         </li>
-      </div>
     );
   }
 };
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import Review from './Review';
-
+import { connect } from 'react-redux';
+ 
 class Reviews extends Component {
+
+  renderReviews = reviews => {
+
+    let restaurantReviews = reviews.filter(review => review.restaurantId === this.props.restaurant.id)
+
+    return (
+      restaurantReviews.map(review => {
+        return <Review review={review} deleteReview={this.props.deleteReview} />
+      })
+    )
+  }
+
   render() {
     return (
       <ul>
-        Reviews
+        {this.renderReviews(this.props.reviews)}
       </ul>
     );
   }
 };
 
-export default Reviews;
+const mapStateToProps = state => {
+  return {
+    reviews: state.reviews
+  }
+}
+
+export default connect(mapStateToProps)(Reviews);
